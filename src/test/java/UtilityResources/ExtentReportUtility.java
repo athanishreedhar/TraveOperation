8c9b6be999f21e089d7a5e34c37ca8827277e9dc
athanishreedhar <shridhara1213@gmail.com>
2020-09-06 14:25:04 +0530

    Update to utility resources by ARch2

--- src/test/java/UtilityResources/ExtentReportUtility.java
+++ src/test/java/UtilityResources/ExtentReportUtility.java
@@ -6,5 +6,14 @@ public class ExtentReportUtility  {
 		System.out.println("Execute some code on start");
 	}
 
+	public void onTestpass() {
+		System.out.println("Test pass code to be written");
+	}
 	
+	public void Testfailure() {
+		System.out.println("Test should be failed ");
+	}
+	public void onSkip() {
+		System.out.println("some code on test skip");
+	}
 }
