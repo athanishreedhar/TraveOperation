15d80b05c56500abd96203c208d21cd8240bcdf6
athanishreedhar <shridhara1213@gmail.com>
2020-09-06 13:39:30 +0530

    new branch drated and comited

---
+++ src/test/java/UtilityResources/ExeclUtility.java
@@ -0,0 +1,11 @@
+package UtilityResources;
+
+public class ExeclUtility {
+public void ExcelInitialize(){
+	System.out.println("initialize excel book");
+}
+
+public void readcelldat() {
+	System.out.println("Reading Cell data");
+}
+}
