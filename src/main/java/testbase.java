08af532a0bbe12a0c50fe28e90d8f15cebd03539
athanishreedhar <shridhara1213@gmail.com>
2020-09-06 11:46:12 +0530

    third commit by Dev

--- src/main/java/testbase.java
+++ src/main/java/testbase.java
@@ -1,4 +1,14 @@
 
 public class testbase {
 
+	public void initialize() {
+		System.out.println("intialize browser");
+	}
+	public void invokeURl() {
+		System.out.println("invoke browser");
+	}
+	
+	public void ReadConfig() {
+		System.out.println("Read config");
+	}
 }
