90886521972938209071722023dcef97e07f5b5b
athanishreedhar <shridhara1213@gmail.com>
2020-09-06 12:24:34 +0530

    sixth commit from DEv

--- src/test/java/testing1.java
+++ src/test/java/testing1.java
@@ -7,4 +7,18 @@ public class testing1 {
 	public void method2() {
 		System.out.println("Hello2");
 	}
+	public void method3() {
+		System.out.println("Hello1");
+	}
+	
+	public void method4() {
+		System.out.println("Hello2");
+	}
+	public void method5() {
+		System.out.println("Hello1");
+	}
+	
+	public void method6() {
+		System.out.println("Hello2");
+	}
 }
